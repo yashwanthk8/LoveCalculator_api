671327b5432a8a9713a545539fc2fd3850fba983
YASHWANTH KUMAR <yashwanthkumar@YASHWANTHs-MacBook-Air.local>
2026-02-28 00:19:23 +0530

    Fix: Remove trailing slash from CORS origin configuration

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://love-calculator-ui.vercel.app/'
+    origins 'https://love-calculator-ui.vercel.app'
 
     resource '*',
       headers: :any,
